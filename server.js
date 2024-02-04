a9fc169c2068039ecc300b47963bf40ff95cc40b
saipavan137 <vbommul1@uncc.edu>
2024-02-04 00:53:05 -0500

    Loading data from JSON file

--- server.js
+++ server.js
@@ -2,27 +2,19 @@ const express = require('express');
 const app = express();
 const port = 3000;
 app.use('/',express.static('public'));
-
-const budget = {
-    mybudget: [
-        {
-            title: 'Eat out',
-            budget: 250
-        },
-        {
-            title: 'Rent',
-            budget: 100
-        },
-        {
-            title: 'Groceries',
-            budget: 300
-        },
-    ]
-};
-
+const fs = require('fs');
 app.get('/budget',(req , res) => {
-    res.json(budget);
-}) 
+    fs.readFile('budget.json', 'utf8', (err, data) => {
+        if (err) {
+            console.error('Error reading JSON file:', err);
+            res.status(500).send('Internal Server Error');
+            return;
+        }
+        const budget = JSON.parse(data);
+        res.json(budget);
+    });
+});
+
 app.listen(port, () => {
     console.log('Example app listening at https://localhost:${port}');
 })
